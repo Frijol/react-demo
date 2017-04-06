7f495726ab06903c57d00ae7eb15aa9814d5d0ca
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2017-04-06 10:46:15 -0700

    render every 1s - this is where difference between JS and React shows up

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Learning React
 
+(Putting this on Github in case I need to find it again)
+
+## Tutorials
+
 created by following this excellent tutorial: <https://medium.freecodecamp.com/yes-react-is-taking-over-front-end-development-the-question-is-why-40837af8ab76>
 
 helped by: <https://medium.com/javascript-scene/how-to-use-es6-for-isomorphic-javascript-apps-2a9c3abe5ea2>
 
 (tl;dr on that is that you should `npm i -g babel-cli` to get started using ES6 elements - which are in all the React tutorials - but do some further installations for anything you're sharing with others)
 
---------------------------------------------------------------------------------
+## What to do
 
-Putting this on Github in case I need to find it again
+`open index.html` to see it in your browser
+
+Try typing something in the JS box while the timer is ticking vs. typing it in the React box.
--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 console.log('hi');
 
 const jsContainer = document.getElementById("js");
-jsContainer.innerHTML = `
-  <div class="demo">
-    Hello JS
-    <input />
-    <p>${new Date()}</p>
-  </div>
-`;
-
 const reactContainer = document.getElementById("react");
-ReactDOM.render(
-  React.createElement(
-    "div", // What kind of element to get
-    { className: "demo" },
-    "Hello React",
-    React.createElement("input"),
+
+const render = () => {
+  jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>${new Date()}</p>
+    </div>
+  `;
+
+  ReactDOM.render(
     React.createElement(
-      "p",
-      null,
-      new Date().toString()
-    )
-  ),
-  reactContainer // Which element to get
-);
+      "div", // What kind of element to get
+      { className: "demo" },
+      "Hello React",
+      React.createElement("input"),
+      React.createElement(
+        "p",
+        null,
+        new Date().toString()
+      )
+    ),
+    reactContainer // Which element to get
+  );
+}
+
+setInterval(render, 1000);
